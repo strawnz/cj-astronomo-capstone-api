05939164327d52b10f1d019f0f9edd4cded02e5f
C.J. Astronomo <cj.astronomo@gmail.com>
2024-02-01 17:43:46 -0500

    Create migrations and seed files for restos/venues samples

---
+++ migrations/20240201213909_create-restos-venues-table.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+    .createTable("restos_venues", (table) => {
+      table.increments("id").primary();
+      table.integer("resto_id").unsigned();
+      table.integer("venue_id").unsigned();
+      table.integer("distance_venue").unsigned();
+      table.integer("duration_venue").unsigned();
+      table.foreign("resto_id").references("id").inTable("restaurants");
+      table.foreign("venue_id").references("id").inTable("venues");
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table
+        .timestamp("updated_at")
+        .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("restos_venues");
+};
